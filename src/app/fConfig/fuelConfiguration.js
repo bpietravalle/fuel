84a406bd01fc73d46c52273f5ccb6e4a99b4f42f
Brian Pietravalle <bpietravalle@gmail.com>
2016-01-16 17:30:50 -0500

    bumped fireStarter dependency and minor edits to fuelConfig factory

--- src/app/fConfig/fuelConfiguration.js
+++ src/app/fConfig/fuelConfiguration.js
@@ -5,6 +5,7 @@
 
     .provider('fuelConfiguration', FuelConfigProvider);
 
+    /** @ngInject */
     function FuelConfigProvider(fireStarterProvider) {
         var prov = this;
         prov.setRoot = function(val) {
@@ -14,18 +15,19 @@
             return fireStarterProvider.getRoot();
         }
 
-        prov.$get = ["fireStarter",
-            function(fireStarter) {
-                switch (angular.isString(prov.getRoot())) {
-                    case true:
-                        return function(type, path, options) {
-                            return fireStarter(type, path, options)
-                        };
-                    case false:
-                        throw new Error("You must define a root url in your module's config block");
-                }
+        prov.$get = fuelProviderGet;
+
+        /** @ngInject */
+        function fuelProviderGet(fireStarter) {
+            switch (angular.isString(prov.getRoot())) {
+                case true:
+                    return function(type, path, options) {
+                        return fireStarter(type, path, options)
+                    };
+                case false:
+                    throw new Error("You must define a root url in your module's config block");
             }
-        ];
+        }
     }
 
 
